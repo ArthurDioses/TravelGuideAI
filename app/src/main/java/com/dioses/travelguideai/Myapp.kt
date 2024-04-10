41cb3032d8a50e1cfc8a2e89fd314c8c78666051
Arthur Dioses <adioses93@gmail.com>
2024-04-10 01:23:41 -0500

    - Create class Application My App - Add new class application in AndroidManifest.xml

---
+++ app/src/main/java/com/dioses/travelguideai/Myapp.kt
@@ -0,0 +1,13 @@
+package com.dioses.travelguideai
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/****
+ * Project: TravelGuideAI
+ * From: com.dioses.travelguideai
+ * Created by Arthur Dioses Reto on 10/04/24 at 12:51 AM
+ * All rights reserved 2024.
+ ****/
+@HiltAndroidApp
+class Myapp : Application()
